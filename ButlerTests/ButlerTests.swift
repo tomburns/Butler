85aa4d9f3773d7cf94ec2e777bb76a9e94959521
Tom Burns <tomburns@gmail.com>
2015-03-14 11:31:46 -0400

    fiddle

--- ButlerTests/ButlerTests.swift
+++ ButlerTests/ButlerTests.swift
@@ -29,7 +29,6 @@ class ButlerTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
-            let foo = 4
             // Put the code you want to measure the time of here.
         }
     }
