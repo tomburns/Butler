02d1ed89599ef3df94513054c9ec9140d8e6ccad
Tom Burns <tomburns@gmail.com>
2015-03-14 11:23:57 -0400

    add junk test

--- ButlerTests/ButlerTests.swift
+++ ButlerTests/ButlerTests.swift
@@ -29,6 +29,7 @@ class ButlerTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
+            let foo = 4
             // Put the code you want to measure the time of here.
         }
     }
